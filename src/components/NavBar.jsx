1453bc0716eca78e19b534d6bbdf5ae0e8d66c71
Angelina Faggiano <adeluca43@gmail.com>
2025-07-30 13:34:44 -0500

    general info page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,43 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function NavBar() {
+export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/info">Info</Link>
-      <Link to="/rules">Rules</Link>
-      <Link to="/brackets">Brackets</Link>
-      <Link to="/results">Results</Link>
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ backgroundColor: "#333" }}
+    >
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse show">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/info">
+                Info
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/rules">
+                Rules
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/brackets">
+                Brackets
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/results">
+                Results
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
