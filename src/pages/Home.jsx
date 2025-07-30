b6f1874f334f5baf7d163de79d76427da4bcd89c
Angelina Faggiano <adeluca43@gmail.com>
2025-07-30 13:18:33 -0500

    inital commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Jiu-Jitsu Tournament</h1>
+      <p>Register, compete, and enjoy the experience!</p>
+    </div>
+  );
+}
---
+++ src/pages/Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Info() {
+  return (
+    <div>
+      <h1>Tournament Information</h1>
+      <ul></ul>
+    </div>
+  );
+}
